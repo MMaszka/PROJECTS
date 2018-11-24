2cfbb25fd2eac485a5704e40ec66d77ad12c06b7
Michal <MichalKMaszka@gmail.com>
2018-11-24 13:57:11 +0100

    bo czemu nie

---
+++ HTML_course/O_nas/O_nas.js
@@ -0,0 +1,19 @@
+function Onas(){
+document.getElementById('opis').innerHTML = '<h1>O nas</h1>' ;
+document.getElementById('michal').innerHTML = '<h1>Michał</h1>';
+}
+
+function michal(){
+document.getElementById('opis').innerHTML = '<h1>ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO ELO </h1>' ;
+document.getElementById('michal').innerHTML = '<h5>lol</h5>';
+}
+
+function bartek(){
+document.getElementById('opis').innerHTML = '<h1>CO MAM ŹLE??!!</h1>' ;
+document.getElementById('bartek').innerHTML = '<h5>O_nas, o_Nas, O_NAS</h5>';
+}
+
+function dominik(){
+document.getElementById('opis').innerHTML = '<h1>CZEŚĆ</h1>' ;
+document.getElementById('dominik').innerHTML = '<h5>co mnie klikasz</h5>';
+}
