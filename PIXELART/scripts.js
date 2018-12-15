2639cbee2daae557b106a2af0a63a9d2e262b95c
Michal <MichalKMaszka@gmail.com>
2018-12-15 13:35:35 +0100

    bo tak

--- PIXELART/scripts.js
+++ PIXELART/scripts.js
@@ -1,5 +1,5 @@
 var penlcolor="black"
-
+var isHeader = 0
 function setPixelColor(pixel)
 {
 	pixel.style.backgroundColor=pencolor;
@@ -18,6 +18,34 @@ function setcurrentColor(color)
 {
 	var currentcolor=document.getElementById("curentcolor");
 	currentColor.style.backgroundColor=color;
+	
+	
+}
+function setText()
+{
+	if (isHeader==0)
+	{
+	document.getElementById("palette").innerHTML = '<h1 id="paletteHeater">Paleta</h1>' + document.getElementById("palette").innerHTML;
+	isHeader=1;
+	}
+	else
+	{
+		document.getElementById("paletteHeater"). remove();
+	isHeader=0;	
+	}
+}
+function addRow()
+{
+
+	var rowCollection = document.getElementsByClassName("row");
+	document.getElementById("Art").innerHTML = document.getElementById("Art").innerHTML + '<div class="row">' + rowCollection[0].innerHTML + '</div>';
+	
+}
+function removeRow()
+{
+	
+	var rowCollection = document.getElementsByClassName("row");
+	rowCollection[0].remove();
 	
 	
 }
